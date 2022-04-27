a947fead9104a74881bc2c457b366d1dab491019
GaoLAQ <61554320+GaoLAQ@users.noreply.github.com>
2022-04-27 21:00:44 +0100

    Update Binary Search.md

--- educative/visualAlgorithms/BinarySearch.js
+++ educative/visualAlgorithms/BinarySearch.js
@@ -3,7 +3,7 @@ let iterativeFunction = function (arr, x) {
     // Iterate while start not meets end
     while (start<=end){
         // Find the mid index
-        let mid=Math.floor((start + end)/2);
+        let mid = Math.floor((start + end)/2);
   
         // If element is present at mid, return True
         if (arr[mid]===x) return mid
@@ -16,4 +16,22 @@ let iterativeFunction = function (arr, x) {
     }
     return -1;
 }
-console.log(iterativeFunction([1,3,5,7,8,9],5))
+
+let recursiveFunction = function(arr, x, left, right){
+  let mid = Math.floor((left+right)/2)
+   if (left < right){
+    if( arr[mid] === x ){
+      return mid 
+    } else if (arr[mid] > left ){
+      recursiveFunction(arr, x, mid +1, right )
+    } else {
+      recursiveFunction(arr, x, left, mid-1)
+    }
+   }
+   return -1 
+}
+
+let arr = [1, 3, 5, 7, 8, 9];
+let x = 5;
+
+console.log(recursiveFunction([1,3,5,7,8,9],x,0,arr.length-1))
